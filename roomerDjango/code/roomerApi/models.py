016957b2f5edfe882ae7bd4c3bbc605d9ca23a9c
MoonLoone <mmaks02@yandex.ru>
2023-03-16 22:23:44 +0300

    Remake Message model

--- roomerDjango/code/roomerApi/models.py
+++ roomerDjango/code/roomerApi/models.py
@@ -61,7 +61,7 @@ class Review(models.Model):
 class Message(models.Model):
     date = models.DateField()
     text = models.CharField(max_length=512)
-    donor = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    recipient = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    donor = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='donor_set')
+    recipient = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='recipient_set')
     isChecked = models.IntegerField()
 
