17855d96979abe9263368c9e7e57d735e7a065ce
MoonLoone <mmaks02@yandex.ru>
2023-03-18 15:36:48 +0300

    Reformat layers

--- roomerDjango/code/roomerBackend/settings.py
+++ roomerDjango/code/roomerBackend/settings.py
@@ -77,7 +77,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("127.0.0.1", 6379)],
+            "hosts": [("0.0.0.0", 6379)],
         },
     },
 }
