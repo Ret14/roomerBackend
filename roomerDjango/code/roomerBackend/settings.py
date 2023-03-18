c9fded598ab2676b79427d0d9cffd39389429940
MoonLoone <mmaks02@yandex.ru>
2023-03-18 14:42:16 +0300

    Add channel layers2

--- roomerDjango/code/roomerBackend/settings.py
+++ roomerDjango/code/roomerBackend/settings.py
@@ -77,7 +77,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("0.0.0.0", 6379)],
+            "hosts": [("127.0.0.1", 6379)],
         },
     },
 }
