94a9954d38484730f1ef7e0287ce65d61519bd46
MoonLoone <mmaks02@yandex.ru>
2023-03-18 14:13:29 +0300

    Delete lobby view

--- roomerDjango/code/roomerBackend/urls.py
+++ roomerDjango/code/roomerBackend/urls.py
@@ -30,7 +30,6 @@ urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('auth/', include('djoser.urls')),
-    path('lobby/', views.lobby),
     re_path(r'^auth/', include('djoser.urls.authtoken')),
 ]
 if settings.DEBUG:
