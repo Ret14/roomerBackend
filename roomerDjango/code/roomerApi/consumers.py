05d4e455d445eb94add1cee0fc4c461a19786a1f
MoonLoone <mmaks02@yandex.ru>
2023-03-21 17:05:04 +0300

    Route

--- roomerDjango/code/roomerApi/consumers.py
+++ roomerDjango/code/roomerApi/consumers.py
@@ -9,9 +9,6 @@ class ChatConsumer(AsyncWebsocketConsumer):
         self.room_group_name = "chat_%s" % self.room_name
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
         await self.accept()
-        await self.channel_layer.group_send(
-            self.room_group_name, {"type": "chat_message", "message": "Hello"}
-        )
 
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
