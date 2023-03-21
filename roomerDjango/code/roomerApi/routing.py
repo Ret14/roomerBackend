5dc5bf89f3bc499064ff12f8d8dbf264367a4141
MoonLoone <mmaks02@yandex.ru>
2023-03-21 14:13:34 +0300

    Routing reformat

--- roomerDjango/code/roomerApi/routing.py
+++ roomerDjango/code/roomerApi/routing.py
@@ -3,5 +3,5 @@ from django.urls import re_path
 from roomerApi import consumers
 
 websocket_urlpatterns = [
-    re_path(r'^ws/chat/(?P<room_name>)/$', consumers.ChatConsumer.as_asgi()),
+    re_path(r'^ws/chat/(?P<id_donor>)/(?P<id_recipient>)', consumers.ChatConsumer.as_asgi()),
 ]
