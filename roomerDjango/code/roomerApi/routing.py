f679c0f30c4d5760407747404f8c1ee8e1207503
MoonLoone <mmaks02@yandex.ru>
2023-03-21 16:18:11 +0300

    Route2

--- roomerDjango/code/roomerApi/routing.py
+++ roomerDjango/code/roomerApi/routing.py
@@ -1,8 +1,7 @@
 
-from django.urls import path
+from django.urls import re_path
 from roomerApi import consumers
 
 websocket_urlpatterns = [
-    path("ws://176.113.83.93:8000/ws/chat/<int:donor_id>/<int:recipient_id>/", consumers.ChatConsumer.as_asgi())
-    #re_path(r"ws/chat/(?P<donor_id>)/(?P<recipient_id>)/$", consumers.ChatConsumer.as_asgi()),
+    re_path(r"ws/chat/(?P<room_name>\w+)/$", consumers.ChatConsumer.as_asgi())
 ]
