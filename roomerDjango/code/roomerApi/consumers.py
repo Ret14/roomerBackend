5920f0ed0d86fb67e1fb427b622a3777f364219b
MoonLoone <mmaks02@yandex.ru>
2023-03-24 19:59:02 +0300

    Remake receive

--- roomerDjango/code/roomerApi/consumers.py
+++ roomerDjango/code/roomerApi/consumers.py
@@ -24,10 +24,10 @@ class ChatConsumer(AsyncWebsocketConsumer):
         recipient_id = text_data_json["recipient_id"]
         donor_profile = Profile.objects.get_queryset().filter(id=donor_id).first()
         recipient_profile = Profile.objects.get_queryset().filter(id=recipient_id).first()
-        message = Message.objects.create(chat_id=donor_id + recipient_id, donor=donor_profile,
-                                         recipient=recipient_profile, text=message)
-        message.save()
-        dict_obj = model_to_dict(message)
+        message_model = Message.objects.create(chat_id=donor_id + recipient_id, donor=donor_profile,
+                                               recipient=recipient_profile, text=message)
+        message_model.save()
+        dict_obj = model_to_dict(message_model)
         serialized = json.dumps(dict_obj)
         await self.channel_layer.group_send(
             self.room_group_name, {"type": "chat_message", "message": serialized}
