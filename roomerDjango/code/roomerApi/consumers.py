dc193f22f6812e120f714a97bd536d09f5c93a50
MoonLoone <mmaks02@yandex.ru>
2023-03-24 10:42:17 +0300

    Test getting messages2

--- roomerDjango/code/roomerApi/consumers.py
+++ roomerDjango/code/roomerApi/consumers.py
@@ -11,11 +11,11 @@ class ChatConsumer(AsyncWebsocketConsumer):
         self.room_group_name = "chat_%d" % (self.donor_id + self.recipient_id)
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
         await self.accept()
-        messages = Message.objects.filter(chat_id=self.donor_id+self.recipient_id).all()
+        messages = Message.objects.filter(chat_id=self.donor_id + self.recipient_id).all()
         serialized_data = serialize("json", messages)
         serialized_data = json.loads(serialized_data)
         await self.channel_layer.group_send(
-            self.room_group_name, {"type": "chat_message", "messages": serialized_data}
+            self.room_group_name, {"type": "chat_message", "message": serialized_data}
         )
 
     async def disconnect(self, close_code):
@@ -30,9 +30,9 @@ class ChatConsumer(AsyncWebsocketConsumer):
         recipient_id = text_data_json["recipient_id"]
         donor_profile = Profile.objects.get_queryset().filter(id=donor_id).first()
         recipient_profile = Profile.objects.get_queryset().filter(id=recipient_id).first()
-        message = Message.objects.create(chat_id=donor_id+recipient_id,donor=donor_profile, recipient=recipient_profile, text=message)
+        message = Message.objects.create(chat_id=donor_id + recipient_id, donor=donor_profile,
+                                         recipient=recipient_profile, text=message)
         message.save()
-
         await self.channel_layer.group_send(
             self.room_group_name, {"type": "chat_message", "message": text_data_json}
         )
