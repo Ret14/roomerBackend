ed3c38a94a36aef24dc880939708d3edf4ee2de2
MoonLoone <mmaks02@yandex.ru>
2023-04-09 18:56:46 +0300

    Rework

--- roomerDjango/code/roomerApi/views.py
+++ roomerDjango/code/roomerApi/views.py
@@ -1,6 +1,7 @@
 from rest_framework import permissions, viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 import datetime
 from roomerApi import serializers
 from roomerApi import models
